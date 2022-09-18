8259134267036cf08751d9e2c5529e36a3697809
openthedidi <openthedidi2004@gmail.com>
2022-09-18 11:01:14 +0800

    『刪除不需要的code』

--- src/main/java/com/example/bank/model/CoinDaoImp.java
+++ src/main/java/com/example/bank/model/CoinDaoImp.java
@@ -1,17 +1,14 @@
 package com.example.bank.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 
 @Component
 public class CoinDaoImp implements CoinDao{
-    @Autowired
-    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     @Autowired
     private CoinRepository coinRepository;
 
@@ -24,13 +21,6 @@ public class CoinDaoImp implements CoinDao{
 
     @Override
     public Coin createCoin(Coin coin) {
-        final String sql = "insert into coin (code,symbol,rate,description,rate_float) values (:code,:symbol,:rate,:description,:rate_float)";
-        Map<String,Object> map = new HashMap<>();
-        map.put("code",coin.getCode());
-        map.put("symbol",coin.getSymbol());
-        map.put("rate",coin.getRate());
-        map.put("description",coin.getDescription());
-        map.put("rate_float",coin.getRate_float());
         Coin newCoin=coinRepository.save(coin);
         return newCoin;
     }
